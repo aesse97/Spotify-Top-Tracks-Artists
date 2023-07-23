e7d225fbeb9951b8368f4b8178769b433cf3b1bb
Drew Esse <andrewesse404@gmail.com>
2023-07-23 14:20:05 -0400

    Static

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -118,6 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+if 'DYNO' in os.environ:  # Check if running on Heroku or a similar platform
+    # Configure STATIC_ROOT for production environment
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
